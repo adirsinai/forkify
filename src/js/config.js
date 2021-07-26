a938b69016c16a068e8a6515378dd1b4c4ee85d3
adirsinai <adirsinai@gmail.com>
2021-07-26 17:36:23 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '96e43972-5002-46b8-96fd-cc2e5b5feef5';
+export const MODAL_CLOSE_SEC = 2.5;
